0c86930bfd155f212f33ffc14a920201b89696b6
Caren-Koroeny <carensiya5@gmail.com>
2023-04-18 05:34:08 +0300

    Made removed default in teacher and student

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, name = 'Unknown', parent_permission: true, specialization)
+  def initialize(age, name, parent_permission, specialization)
     super(age, name, parent_permission)
     @specialization = specialization
   end
