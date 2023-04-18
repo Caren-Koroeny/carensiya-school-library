7ffeb800410403ef282b692cd85c75b90efc2176
Caren-Koroeny <carensiya5@gmail.com>
2023-04-18 04:24:49 +0300

    Fix linters Errors

--- person.rb
+++ person.rb
@@ -1,23 +1,21 @@
 class Person
-    attr_accessor :name, :age
-    attr_reader :id
-  
-    def initialize(age, name = "Unknown", parent_permission: true)
-      @id = "#{Random.new().rand(1000)}-#{Random.new().rand(1000)}-#{Random.new().rand(1000)}"
-      @age = age
-      @name = name
-      @parent_permission = parent_permission
-     
-    end
-  
-    def of_age?
-      @age >= 18
-    end
-  
-    private
-  
-    def can_use_services?
-      (@age >= 18 || @parent_permission) ? true : false
-    end
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = "#{Random.new.rand(1000)}-#{Random.new.rand(1000)}-#{Random.new.rand(1000)}"
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
   end
-  
+
+  def of_age?
+    @age >= 18
+  end
+
+  private
+
+  def can_use_services?
+    @age >= 18 || @parent_permission ? true : false
+  end
+end
